d1e36ed43f4452c304e3e45c974dc805b4d68120
Jk Xu <54522439+Dousir9@users.noreply.github.com>
2021-06-25 19:20:26 +0800

    修复了创建视图类题目时存在的缺陷

--- handler.py
+++ handler.py
@@ -45,7 +45,17 @@ def judge_handler(fingerprint, code, checker, cases, table, problem_type, ):
             if problem_type == '更新类':
                 table_select_sql = 'select * from ' + table['table_to_do'][0] + ';'
             elif problem_type == '创建视图类':
-                view_to_select = re.findall(r'^\s*create\s+view\s+(\S+)\s*', code, re.I)
+                view1 = re.findall(r'^\s*create\s+view\s+(\S+)\s*', code, re.I)
+                view2 = re.findall(r'^\s*create\s+view\s+(\S+)\(', code, re.I)
+                if len(view1) > 0 and len(view2) > 0:
+                    if len(view1[0]) < len(view2[0]):
+                        view_to_select = view1
+                    else:
+                        view_to_select = view2
+                elif len(view1) > 0:
+                    view_to_select = view1
+                else:
+                    view_to_select = view2
                 if view_to_select and view_to_select[0] == table['table_to_do'][0]:
                     view_delete_sql = 'drop view ' + table['table_to_do'][0] + ';'
                     table_select_sql = 'select * from ' + table['table_to_do'][0] + ';'
